711d445be91c76ac14da056d416f93a0d85c44d6
Cedric Joulain <cedric.joulain@strigi-form.fr>
2020-11-23 10:07:00 +0100

    add: fit fn with model as argument

--- Python/Modele/TCN/CJ/predict_tcn.py
+++ Python/Modele/TCN/CJ/predict_tcn.py
@@ -25,34 +25,10 @@ HISTORY_LAG = 3
 DELTA = False
 
 def main():
-    """TCN fit and test"""
-    #Scale the all of the data to be values between 0 and 1
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    serie = uat.get_array(uat.TEMPERATURE, WEATHER_STA, START, STOP)
-    serie = serie.transpose()
-    if DELTA:
-        features = serie[1:] - serie[:-1]
-    else:
-        features = serie
+    rmse = fit_tcn()
+    print("TCN RMSE:", rmse)
 
-    print(features.shape)
-    scaled_data = scaler.fit_transform(features)
-
-    training_dataset_length = math.ceil(len(features) * .75)
-
-    train_data = scaled_data[0:training_dataset_length, : ]
-
-    #Splitting the data
-    x_train = []
-    y_train = []
-    for i in range(HISTORY_LAG, len(train_data)):
-        x_train.append(train_data[i-HISTORY_LAG:i])
-        y_train.append(train_data[i])
-    x_train = np.array(x_train)
-    y_train = np.array(y_train)
-    print(x_train.shape)
-    print(y_train.shape)
-
+def fit_tcn(verbose=1, epochs=200, batch_size=16):
     i = Input(shape=(HISTORY_LAG, 1))
     layer = TCN(nb_filters=64,
                 kernel_size=2,
@@ -70,10 +46,41 @@ def main():
     layer = Dense(1)(layer)
 
     model = Model(inputs=[i], outputs=[layer])
+    return fit(model, verbose=verbose, epochs=epochs, batch_size=batch_size)
+
+def fit(model, verbose=1, epochs=200, batch_size=16):
+    """Fit model and return RMSE on test data"""
+    #Scale the all of the data to be values between 0 and 1
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    serie = uat.get_array(uat.TEMPERATURE, WEATHER_STA, START, STOP)
+    serie = serie.transpose()
+    if DELTA:
+        features = serie[1:] - serie[:-1]
+    else:
+        features = serie
+    if verbose > 0:
+        print(features.shape)
+    scaled_data = scaler.fit_transform(features)
+
+    training_dataset_length = math.ceil(len(features) * .75)
+
+    train_data = scaled_data[0:training_dataset_length, : ]
+
+    #Splitting the data
+    x_train = []
+    y_train = []
+    for i in range(HISTORY_LAG, len(train_data)):
+        x_train.append(train_data[i-HISTORY_LAG:i])
+        y_train.append(train_data[i])
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    if verbose > 0:
+        print(x_train.shape)
+        print(y_train.shape)
 
     model.compile(optimizer='adam', loss='mean_squared_error')
-
-    model.fit(x_train, y_train, epochs=200, batch_size=16)
+    model.fit(x_train, y_train,
+              epochs=epochs, batch_size=batch_size, verbose=verbose)
 
     #Test data set
     test_data = scaled_data
@@ -101,19 +108,21 @@ def main():
         predict += serie[HISTORY_LAG:-1, :]
 
     #Calculate RMSE score
-    print("use previous RMSE:",
-          rmse(previous, y_test, training_dataset_length, HISTORY_LAG))
-    print("predicted RMSE:",
-          rmse(predict, y_test, training_dataset_length, HISTORY_LAG))
-    print("previous vs predicted RMSE:",
-          rmse(predict, previous, training_dataset_length, HISTORY_LAG))
+    result = rmse(predict, y_test, training_dataset_length, HISTORY_LAG)
+    if verbose == 1:
+        print("use previous RMSE:",
+              rmse(previous, y_test, training_dataset_length, HISTORY_LAG))
+        print("predicted RMSE:", rmse)
+        print("previous vs predicted RMSE:",
+              rmse(predict, previous, training_dataset_length, HISTORY_LAG))
 
-    plt.figure(1)
-    plt.plot(predict, color='red', label='predicted', linewidth=1.0)
-    plt.plot(y_test, color='blue', label='actual', linewidth=1.0)
-    plt.axvline(x=training_dataset_length-HISTORY_LAG)
-    plt.legend(['predicted', 'actual'])
-    plt.show()
+        plt.figure(1)
+        plt.plot(predict, color='red', label='predicted', linewidth=1.0)
+        plt.plot(y_test, color='blue', label='actual', linewidth=1.0)
+        plt.axvline(x=training_dataset_length-HISTORY_LAG)
+        plt.legend(['predicted', 'actual'])
+        plt.show()
+    return result
 
 def rmse(predict, ref, training_length, lag):
     """Compute Root Mean Square Error after training data"""
