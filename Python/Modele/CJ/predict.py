192108a90fd21ec09ae6ddbc4e7f72c0f8a32dda
Cedric Joulain <cedric.joulain@strigi-form.fr>
2020-11-25 10:51:14 +0100

    add: LSTM available

--- Python/Modele/CJ/predict.py
+++ Python/Modele/CJ/predict.py
@@ -13,6 +13,9 @@ import matplotlib.pyplot as plt
 from geneticalgorithm import geneticalgorithm as ga
 
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
 import tcn
 
 import array_from_uat as uat
@@ -25,17 +28,23 @@ HISTORY_LAG = 8
 DELTA = False
 
 def main():
-    rmse_hist()
+    """Main, first to be called"""
+    fit_lstm(verbose=1)
 
-def tcn_genetic():
-    """TCN input/architecture genetic optimization"""
-    varbound=np.array(
-        [[1,6], #histo lag 3, 4, 8, 16, 32, 64
-        [4,6],  #nb_filters 16, 32, 64
-        [2,8],  #kernel size from 2 to 8
-        [1,3] #nb_stacks
+def hist_lstm():
+    """RMSE stability for LSTM"""
+    #/2 as hyper threading is probably on
+    with mp.Pool(int(mp.cpu_count()/2)) as pool:
+        rmses = np.array(pool.map(fit_lstm, [HISTORY_LAG]*8))
+    print("RMSE avg:", np.mean(rmses), "min:", np.amin(rmses), "sd:", np.std(rmses))
+
+def genetic_lstm():
+    """LSTM input/architecture genetic optimization"""
+    varbound = np.array(
+        [[3, 64], #histo
+         [16, 64]   #units
         ])
-    algorithm_parameters={
+    algorithm_parameters = {
         'max_num_iteration': None,
         'population_size':100, #default is 100
         'mutation_probability':0.1,
@@ -44,8 +53,60 @@ def tcn_genetic():
         'parents_portion': 0.3,
         'crossover_type':'uniform',
         'max_iteration_without_improv':None}
-    optim=ga(
-        function=tcn_rmse,
+    optim = ga(
+        function=rmse_lstm,
+        dimension=2,
+        variable_type='int',
+        variable_boundaries=varbound,
+        function_timeout=60*60,
+        algorithm_parameters=algorithm_parameters)
+    optim.run()
+
+def rmse_lstm(arr):
+    """Compte RMSE from LSTM described by a numpy array"""
+    return fit_lstm(lag=int(arr[0]), units=int(arr[1]))
+
+def fit_lstm(lag=HISTORY_LAG, units=32, verbose=0, epochs=100, batch_size=16):
+    """Learn LSTM"""
+    model = Sequential()
+    model.add(LSTM(
+        units=units,
+        return_sequences=False,
+        recurrent_dropout=0.2,
+        input_shape=(lag, 1)))
+
+    # Adding the output layer
+    # For Full connection layer we use dense
+    # As the output is 1D so we use unit=1
+    model.add(Dense(units=1))
+    #################################################
+
+    #compile and fit the model on 10 epochs
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    result = fit(model, lag=lag, verbose=verbose, epochs=epochs, batch_size=batch_size)
+    print("LSTM lag:", lag, "units:", units, "rmse:", result)
+    return result
+
+
+def genetic_tcn():
+    """TCN input/architecture genetic optimization"""
+    varbound = np.array(
+        [[1, 6], #histo lag 3, 4, 8, 16, 32, 64
+         [4, 6],  #nb_filters 16, 32, 64
+         [2, 8],  #kernel size from 2 to 8
+         [1, 3] #nb_stacks
+        ])
+    algorithm_parameters = {
+        'max_num_iteration': None,
+        'population_size':100, #default is 100
+        'mutation_probability':0.1,
+        'elit_ratio': 0.01,
+        'crossover_probability': 0.5,
+        'parents_portion': 0.3,
+        'crossover_type':'uniform',
+        'max_iteration_without_improv':None}
+    optim = ga(
+        function=rmse_tcn,
         dimension=4,
         variable_type='int',
         variable_boundaries=varbound,
@@ -53,7 +114,7 @@ def tcn_genetic():
         algorithm_parameters=algorithm_parameters)
     optim.run()
 
-def tcn_rmse(arr):
+def rmse_tcn(arr):
     """Compte RMSE from TCN described by a numpy array"""
     #histo lag 3, 4, 8, 16, 32, 64, 128, 256
     lag = 2**arr[0]
@@ -65,24 +126,29 @@ def tcn_rmse(arr):
         kernel_size=int(arr[2]),
         nb_stacks=int(arr[3]))
 
-def rmse_hist():
+def hist_tcn():
+    """RMSE stability for TCN"""
     #/2 as hyper threading is probably on
-    with mp.Pool(int(mp.cpu_count()/2)) as p:
-        rmses = np.array(p.map(fit_tcn, [HISTORY_LAG]*8))
+    with mp.Pool(int(mp.cpu_count()/2)) as pool:
+        rmses = np.array(pool.map(fit_tcn, [HISTORY_LAG]*8))
     print("RMSE avg:", np.mean(rmses), "min:", np.amin(rmses), "sd:", np.std(rmses))
 
-def fit_tcn(lag=HISTORY_LAG, nb_filters=32, kernel_size=5, nb_stacks=1, verbose=0, epochs=100, batch_size=16):
+def fit_tcn(lag=HISTORY_LAG, nb_filters=32, kernel_size=5, nb_stacks=1,
+            verbose=0, epochs=100, batch_size=16):
+    """Learn TCN"""
     #dilations can be infered from input size (lag)
-    dilations=[2**i for i in range(0, int(math.log(lag)/math.log(2)))]
+    dilations = [2**i for i in range(0, int(math.log(lag)/math.log(2)))]
     model = tcn.compiled_tcn(
         1, # num_feat
         1, # num_classes
         nb_filters, kernel_size, dilations, nb_stacks, lag, regression=True)
-    result = fit(model, lag=lag, verbose=verbose, epochs=epochs, batch_size=batch_size)
-    print("TCN lag:", lag, "units:", nb_filters, "ksize:", kernel_size, "stacks:", nb_stacks, "rmse:", result)
+    result = fit(model, lag=lag, verbose=verbose,
+                 epochs=epochs, batch_size=batch_size, cut_predict=True)
+    print("TCN lag:", lag, "units:", nb_filters, "ksize:", kernel_size,
+          "stacks:", nb_stacks, "rmse:", result)
     return result
 
-def fit(model, lag=HISTORY_LAG, verbose=1, epochs=200, batch_size=16):
+def fit(model, lag=HISTORY_LAG, verbose=1, epochs=200, batch_size=16, cut_predict=False):
     """Fit model and return RMSE on test data"""
     #Scale the all of the data to be values between 0 and 1
     scaler = MinMaxScaler(feature_range=(0, 1))
@@ -135,7 +201,8 @@ def fit(model, lag=HISTORY_LAG, verbose=1, epochs=200, batch_size=16):
     #check predicted values
     predict = model.predict(x_test)
     #keep only next predicted value
-    predict = predict[:,-1]
+    if cut_predict:
+        predict = predict[:, -1]
     #Undo scaling
     predict = scaler.inverse_transform(predict)
     #add back previous point
