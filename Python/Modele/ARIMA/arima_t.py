4a172c854c0b30b129e07bfd7914d85a36700d7a
Drif Riadh <60885440+dux7@users.noreply.github.com>
2020-11-26 16:11:39 +0100

    Update arima_t.py

--- Python/Modele/ARIMA/arima_t.py
+++ Python/Modele/ARIMA/arima_t.py
@@ -1,7 +1,8 @@
+# conda install statsmodels.api
+
 import warnings
 import itertools
 import pandas as pd
-import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
