a96092f168f3b60c4eb73f1408bee6e5bf139bce
Drif Riadh <60885440+dux7@users.noreply.github.com>
2020-11-26 15:37:48 +0100

    Add files via upload
    Code Prédiction ARIMA

---
+++ Python/Modele/ARIMA/arima_t.py
@@ -0,0 +1,85 @@
+import warnings
+import itertools
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+
+
+
+
+WEATHER_STA = 14578001
+
+df = pd.read_csv("NW_Ground_Stations_2017.csv")
+weather = df[(df['number_sta'] == WEATHER_STA)]
+weather['date'] = pd.to_datetime(df['date'])
+weather = weather.set_index('date')
+weather = weather['2017-01-01':'2017-1-12'].resample('6min').sum()
+#weather = weather.resample('1H').mean()
+#weather = weather.fillna(weather['t'].bfill())
+print(weather['t'])
+
+plt.ylim(260,320)
+plt.plot(weather['t'])
+plt.show()
+# weather['t'].plot(figsize=(20, 6))
+# plt.show()
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+
+warnings.filterwarnings("ignore")
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(weather['t'],
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+
+            results = mod.fit()
+
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+        
+warnings.filterwarnings("ignore")        
+mod = sm.tsa.statespace.SARIMAX(weather['t'],
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 1, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+print(results.summary().tables[1])
+
+results.plot_diagnostics(figsize=(20, 12))
+plt.show()
+
+pred = results.get_prediction(start=pd.to_datetime('2017-01-12'), dynamic=False)
+pred_ci = pred.conf_int()
+
+ax = weather['t']['2017-01-01':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)
+
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Temperature')
+plt.legend()
+plt.ylim(260,320)
+plt.show()
